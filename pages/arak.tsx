e11aac711e548b610a02105ef9cfdbf3ffb10d21
Donat Harajda <harajdadoni@gmail.com>
2022-12-13 14:57:48 +0100

    commit from szetnmiklos 1213

--- pages/arak.tsx
+++ pages/arak.tsx
@@ -1,3 +1,58 @@
-const Arak = () => {};
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+const Arak = () => {
+   return (
+     <>
+    <section className="bg-nk-black h-full p-5">
+        <Navbar />
+        <div className="container mx-auto flex flex-col gap-5">
+            <h1 className="text-4xl font-bold text-nk-white uppercase">áraink</h1>
+            <hr className="border-nk-red border-solid rounded-md w-36"/>
+            <h2 className="uppercase text-2xl text-nk-white tracking-widest">a legjobb verziód itt készül</h2>
+        </div>
+        <div className="container mx-auto flex flex-col">
+            <p className="text-xl text-nk-white">Mielőtt belevágunk a közös munkába, szeretettel látunk egy díjmentes találkozón, ahol rólad, a te céljaidról, terveidről lesz szó. Átbeszéljük, milyen típusú edzésmód lenne számodra ideális.
+            </p>
+            <hr className="border-nk-red border-solid rounded-md w-36"/>
+        </div>
+        <div className="container mx-auto flex justify-between">
+            <div className="w-1/3 bg-nk-white rounded-md flex flex-col items-center">
+                <h2>személyi edzés</h2>
+                <h1>egyéni</h1>
+                <p>
+                    6000.-Ft / fő / <span className="text-nk-red font-bold">1</span> alkalom
+                    <hr className="border-nk-red border-solid rounded-md w-10" />
+                </p>
+                <p>
+                    6000.-Ft / fő / <span className="text-nk-red font-bold">1</span> alkalom
+                    <hr className="border-nk-red border-solid rounded-md w-10" />
+                </p>
+                <p>
+                    6000.-Ft / fő / <span className="text-nk-red font-bold">1</span> alkalom
+                    <hr className="border-nk-red border-solid rounded-md w-10" />
+                </p>
+            </div>
+            <div className="w-1/3 h-50 bg-nk-white rounded-md flex flex-col items-center">
+                <h2>személyi edzés</h2>
+                <h1>egyéni</h1>
+                <p>
+                    6000.-Ft / fő / <span className="text-nk-red font-bold">1</span> alkalom
+                    <hr className="border-nk-red border-solid rounded-md w-10" />
+                </p>
+                <p>
+                    6000.-Ft / fő / <span className="text-nk-red font-bold">1</span> alkalom
+                    <hr className="border-nk-red border-solid rounded-md w-10" />
+                </p>
+                <p>
+                    6000.-Ft / fő / <span className="text-nk-red font-bold">1</span> alkalom
+                    <hr className="border-nk-red border-solid rounded-md w-10" />
+                </p>
+            </div>
+        </div>
+    </section>
+    <Footer />
+    </>
+   )
+};
 
 export default Arak;
---
+++ pages/edzok.tsx
@@ -0,0 +1,43 @@
+import Navbar from "../components/Navbar";
+import Image from "next/image";
+import Footer from "../components/Footer";
+
+const Edzok = () => {
+    return(
+    <>
+        <section className="bg-nk-black h-full">
+            <Navbar />
+            <div className="container mx-auto flex flex-col p-5">
+                <h1 className="text-4xl text-nk-white font-bold uppercase">edzők</h1>
+                <hr className="border-nk-red border-solid rounded-md w-24"></hr>
+            </div>
+            <div className="container mx-auto text-nk-white flex flex-wrap p-5">
+                <div className="flex flex-col basis-1/2 pb-10">
+                    <Image src="/../public/images/Kevin.jpg" alt="Trainer Kevin" width={400} height={400} className="rounded-xl"></Image>
+                    <h1>Varga Kevin</h1>
+                    <small>személyi edző</small>
+                </div>
+                <div className="flex flex-col basis-1/2 pb-10">
+                    <Image src="/../public/images/Norbi.jpg" alt="Trainer Norbi" width={400} height={400} className="rounded-xl"></Image>
+                    <h1>Baranyai Norbert</h1>
+                    <small>személyi edző</small>
+                </div>
+                <div className="flex flex-col basis-1/2">
+                    <Image src="/../public/images/Adrienn.jpg" alt="Trainer Adrienn" width={400} height={400} className="rounded-xl"></Image>
+                    <h1>Ivancsics Adrienn</h1>
+                    <small>szeméyi edző</small>
+                </div>
+                <div className="flex flex-col basis-1/2">
+                    <Image src="/../public/images/Judit.jpg" alt="Trainer Judit" width={400} height={400} className="rounded-xl"></Image>
+                    <h1>Palotai Fodor Judit</h1>
+                    <small>személyi edző</small>
+                </div>
+            </div>
+        </section>
+        <Footer />
+        </>
+    )
+}
+
+
+export default Edzok;
