43770eb6e9adf075396f6a0ba2d3aae55daa22ba
Donat Harajda <harajdadoni@gmail.com>
2023-03-06 21:06:50 +0100

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ module.exports = {
       backgroundImage: {
         logo_image: "url('/images/middle3.jpg')",
         "hero-pattern_2048": "url('/images/background.png')",
-        "hero-pattern_768": "url('/images/background_1_768x1024.png')",
+        "hero-pattern_768": "url('/images/background_1_768x1024.webp')",
         rolunk_bg: "url('/images/feature2bg.jpg')",
         rolunk_second: "url('/images/feature2_2nd.jpg')",
         bejelentkezes: "url('/images/bejelentkezes.png')",
