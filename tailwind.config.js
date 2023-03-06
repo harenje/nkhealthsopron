698236a5a63eb9a7d9b19f5d3c626c2ccf0afdef
Donat Harajda <harajdadoni@gmail.com>
2023-03-06 12:10:29 +0100

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,14 @@ const Image = require("next/image");
 const { fontFamily } = require("tailwindcss/defaultTheme");
 
 const backgroundImage = (imageUrl) => ({
-  backgroundImage: `url(${Image(imageUrl)}`,
+  backgroundImage: `url(${imageUrl})`,
 });
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
+    "/images/background_1_768x1024.png"("/images/bejelentkezes.png"),
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
@@ -17,13 +18,23 @@ module.exports = {
       screens: {},
       backgroundImage: {
         logo_image: "url('/images/middle3.jpg')",
-        "hero-pattern_2048": backgroundImage("/images/background.png"),
+        "hero-pattern_2048": backgroundImage(
+          Image.loader("/images/background.png", {
+            deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+          })
+        ),
         "hero-pattern_768": backgroundImage(
-          "/images/background_1_768x1024.png"
+          Image.loader("/images/background_1_768x1024.png", {
+            deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+          })
         ),
         rolunk_bg: "url('/images/feature2bg.jpg')",
         rolunk_second: "url('/images/feature2_2nd.jpg')",
-        bejelentkezes: backgroundImage("/images/bejelentkezes.png"),
+        bejelentkezes: backgroundImage(
+          Image.loader("/images/bejelentkezes.png", {
+            deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+          })
+        ),
         training_gif: "url('/images/training_gif.gif')",
       },
       colors: {
