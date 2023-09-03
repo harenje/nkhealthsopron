4f78897f2ef0a988332fcaedb89dfc0b75977b48
Donat Harajda <harajdadoni@gmail.com>
2023-09-03 18:55:41 +0200

    .

--- pages/hirfolyam.tsx
+++ pages/hirfolyam.tsx
@@ -83,7 +83,7 @@ export const getStaticProps: GetStaticProps = async () => {
 
     if (!response.ok) {
       throw new Error(
-        `Failed to fetcha from Instagram. Status: ${response.status}`
+        `Failed to fetchaa from Instagram. Status: ${response.status}`
       );
     }
 
